b12c186f7b52a804e7e3458ef6fdd5ce11ee1f9e
Aaron Wee <aaron@aaronwee.com>
2012-05-12 00:16:34 +0800

    initial commit with refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rsvp::Application.config.secret_token = 'a670c80fbee6c0d60357fc4bda0d4244752fae37d872a5084d64fcb6e1fa3fb10dbd43fc8141d1b4dd8ce103eae59e41dab2ac415cc498b3687d677479f6ea8e'
